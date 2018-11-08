126dbf02a5254b3346fbd4d6bda698b62c978ce5
Chris Davie <cjdavie@googlemail.com>
2018-11-08 10:03:30 +0000

    Implement binary search for length of list between bounds

--- length_of_list/length_of_list.py
+++ length_of_list/length_of_list.py
@@ -5,11 +5,26 @@ given index (assuming and error when invalid indices are used)
 LOOP_LIMIT = 10000
 
 
+def length_of_list(target_list: list) -> int:
+    """
+    Finds the length of a list.
+
+    The 'list' can only be accessed at index <list>[index] and throws an
+    IndexError on invalid incides.
+    """
+    upper_bound, lower_bound = find_upper_lower_bounds_list(target_list)
+    return find_list_length_between_bounds(
+        target_list, upper_bound, lower_bound)
+
+
 def find_upper_lower_bounds_list(target_list: list) -> (int, int):
     """
-    Find the upper and lower bounds of a list that can be accessed at index
-    <list>[index] and throws an error on invalid incides
+    Find the upper and lower bounds of the length of a 'list'
+
+    The 'list' can only be accessed at index <list>[index] and throws an
+    IndexError on invalid incides.
     """
+    # search upwards in 2^n jumps
     lower_bound = 0
     for index in range(LOOP_LIMIT):
         two_power_index = 2 ** index
@@ -23,3 +38,39 @@ def find_upper_lower_bounds_list(target_list: list) -> (int, int):
             f"Target list length exceeds maximum {2**(LOOP_LIMIT-1)}")
 
     return lower_bound, two_power_index
+
+
+def find_list_length_between_bounds(
+        target_list: list, lower_bound: int, upper_bound: int) -> int:
+    """
+    Binary search for the length of a list within lower_bound and upper_bound.
+
+    The 'list' can only be accessed at index <list>[index] and throws an
+    IndexError on invalid incides.
+
+    Assumes lower_bound is a valid index and upper_bound raises an exception,
+    with the special case of lower_bound of 0, which can raise if the list is
+    of zero length.
+    """
+    # Handle zero length
+    if lower_bound == 0:
+        try:
+            target_list[lower_bound]
+        except IndexError:
+            return lower_bound
+
+    # Binary search between bounds
+    for _ in range(LOOP_LIMIT):
+        if lower_bound + 1 == upper_bound:
+            break
+        midpoint = (lower_bound + upper_bound) // 2
+        try:
+            target_list[midpoint]
+            lower_bound = midpoint
+        except IndexError:
+            upper_bound = midpoint
+    else:
+        raise ValueError(
+            f"List length not found within maximum iterations {LOOP_LIMIT}")
+
+    return upper_bound
--- length_of_list/test.py
+++ length_of_list/test.py
@@ -3,12 +3,27 @@ from unittest.mock import patch
 
 from parameterized import parameterized
 
-from length_of_list.length_of_list import find_upper_lower_bounds_list
+from length_of_list.length_of_list import find_upper_lower_bounds_list, \
+    find_list_length_between_bounds, length_of_list
+
+
+class TestLengthOfList(TestCase):
+    """Test full run"""
+
+    @parameterized.expand(((i,) for i in range(20)))
+    def test(self, list_length):
+
+        target_list = [1] * list_length
+        self.assertEqual(list_length, length_of_list(target_list))
+
+
+LIST_LENGTHS_WITH_UPPER_LOWER_BOUNDS = [
+    (0, 0, 1), (1, 0, 1), (2, 1, 2), (11, 8, 16)]
 
 
 class TestFindUpperLowerBoundsList(TestCase):
 
-    @parameterized.expand([(0, 0, 1), (2, 1, 2), (11, 8, 16)])
+    @parameterized.expand(LIST_LENGTHS_WITH_UPPER_LOWER_BOUNDS)
     def test(self, list_length, expected_lower_bound, expected_upper_bound):
 
         target_list = [1] * list_length
@@ -26,3 +41,27 @@ class TestFindUpperLowerBoundsList(TestCase):
             target_list = [1] * (2 ** (patched_loop_limit - 1) + 1)
             with self.assertRaises(ValueError):
                 find_upper_lower_bounds_list(target_list)
+
+
+class TestFindListLengthBetweenBounds(TestCase):
+
+    @parameterized.expand(LIST_LENGTHS_WITH_UPPER_LOWER_BOUNDS)
+    def test(self, list_length, lower_bound, upper_bound):
+        target_list = [1] * list_length
+
+        reported_list_length = find_list_length_between_bounds(
+            target_list, lower_bound, upper_bound)
+
+        self.assertEqual(reported_list_length, list_length)
+
+    def test_raises_when_exceeds_loop_limit(self):
+
+        target_list = [1] * 11
+        lower_bound = 8
+        upper_bound = 16
+
+        patched_loop_limit = 1
+        with patch("length_of_list.length_of_list.LOOP_LIMIT",
+                   patched_loop_limit):
+            with self.assertRaises(ValueError):
+                find_list_length_between_bounds(target_list, lower_bound, upper_bound)
